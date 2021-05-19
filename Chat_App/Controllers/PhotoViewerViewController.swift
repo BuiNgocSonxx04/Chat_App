4eefb459e2eae1059d45fc01d54ef2be4a586701
BuiNgocSon <sonbeat1204@gmail.com>
2021-05-19 21:41:10 +0700

    Getting Started

---
+++ Chat_App/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewerViewController.swift
+//  Chat_App
+//
+//  Created by BuiNgocSon on 5/12/21.
+//  Copyright © 2021 BuiNgocSon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
